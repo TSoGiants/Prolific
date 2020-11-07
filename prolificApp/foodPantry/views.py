99d7807ad68d311ec398dca98a54f05768d14953
styx-sudo <62368485+styx-sudo@users.noreply.github.com>
2020-11-07 17:11:32 -0500

    Created routes/endpoints
    for the about, directory, and food bank profile pages

--- prolificApp/foodPantry/views.py
+++ prolificApp/foodPantry/views.py
@@ -5,3 +5,13 @@ foodPantry_app = Blueprint('foodPantry', __name__)
 @foodPantry_app.route('/')
 def index():
 	return "landing page"
+@foodPantry_app.route('/About')
+def index():
+	return "About"
+@foodPantry_app.route('/Directory')
+def index():
+	return "Directory"
+@foodPantry_app.route('/Food pantry profile')
+def index():
+	return "Food pantry profile"
+
