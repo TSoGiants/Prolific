89fb2111c7529d980ff6d1c05c309766b1def863
styx-sudo <62368485+styx-sudo@users.noreply.github.com>
2020-11-07 20:22:55 -0500

    Update views.py

--- prolificApp/foodPantry/views.py
+++ prolificApp/foodPantry/views.py
@@ -5,13 +5,13 @@ foodPantry_app = Blueprint('foodPantry', __name__)
 @foodPantry_app.route('/')
 def index():
 	return "landing page"
-@foodPantry_app.route('/About')
+@foodPantry_app.route('/about')
 def about():
 	return "About"
-@foodPantry_app.route('/Directory')
+@foodPantry_app.route('/directory')
 def directory():
 	return "Directory"
-@foodPantry_app.route('/Food pantry profile')
+@foodPantry_app.route('/food pantry profile')
 
 def fpProfile():
 	return "Food pantry profile"
