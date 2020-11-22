65187a99254da9ad08b8f9b669e2873aa45c81c4
sauravroutt <73410713+sauravroutt@users.noreply.github.com>
2020-11-21 20:13:06 -0600

    created routes for user package

--- prolificApp/user/views.py
+++ prolificApp/user/views.py
@@ -1,7 +1,16 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, render_template
 
 user_app = Blueprint('Users', __name__)
 
 @user_app.route('/login')
 def login():
-	return "login Page"
+	return render_template('login.html')
+@user_app.route('/logout')
+def logout():
+	return render_template('logout.html')	
+@user_app.route('/register')
+def register():
+	return render_template('register.html')
+@user_app.route('/profile')
+def profile():
+	return render_template('profile.html')	
