f30df19976bfc4ffe45daf8203742394dd15424c
ayaanm0703 <ayaanm0703@gmail.com>
2020-11-28 20:38:44 -0600

    Added /registerFB and /registerGU

--- prolificApp/user/views.py
+++ prolificApp/user/views.py
@@ -7,10 +7,13 @@ def login():
 	return render_template('login.html')
 @user_app.route('/logout')
 def logout():
-	return render_template('logout.html')	
-@user_app.route('/register')
-def register():
-	return render_template('register.html')
+	return render_template('logout.html')
+@user_app.route('/registerFB')
+def registerFB():
+	return render_template('registerFB.html')
+@user_app.route('/registerGU')
+def registerGU():
+	return render_template('registerGU.html')
 @user_app.route('/profile')
 def profile():
-	return render_template('profile.html')	
+	return render_template('profile.html')
