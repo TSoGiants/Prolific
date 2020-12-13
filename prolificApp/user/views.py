a26b48195878ce19374dae03bd77f4ee0961de7a
ayaanm0703 <ayaanm0703@gmail.com>
2020-12-12 20:29:53 -0600

    Created general user and food pantry sign up forms

--- prolificApp/user/views.py
+++ prolificApp/user/views.py
@@ -8,12 +8,15 @@ def login():
 @user_app.route('/logout')
 def logout():
 	return render_template('logout.html')
-@user_app.route('/registerFB')
-def registerFB():
-	return render_template('registerFB.html')
+@user_app.route('/registerFP')
+def registerFP():
+	return render_template('registerFP.html')
 @user_app.route('/registerGU')
 def registerGU():
 	return render_template('registerGU.html')
 @user_app.route('/profile')
+@user_app.route('/editprofile')
+def editprofile():
+	return render_template('editprofile.html')
 def profile():
 	return render_template('profile.html')
