a2112cfca8fbc085a8c1ce054536565b01102a44
ayaanm0703 <ayaanm0703@gmail.com>
2020-12-19 19:17:16 -0600

    Added index.html

--- prolificApp/foodPantry/views.py
+++ prolificApp/foodPantry/views.py
@@ -4,25 +4,22 @@ foodPantry_app = Blueprint('foodPantry', __name__)
 
 @foodPantry_app.route('/')
 def index():
-	return render_template('Landing-page.html')
+	return render_template('index.html')
 
-	
+
 @foodPantry_app.route('/about')
 def about():
 	return render_template('About.html')
-#creates endpoint for the About webpage 
+#creates endpoint for the About webpage
 
 
 @foodPantry_app.route('/directory')
 def directory():
 	return render_template('Directory.html')
-#creates endpoint for the directory webpage 
+#creates endpoint for the directory webpage
 
 @foodPantry_app.route('/food pantry profile')
-def fpProfile(): 
+def fpProfile():
 	return render_template('FoodPantryProfile.html')
 
-#creates endpoint for the food pantry profile page webpage 
-
-
-
+#creates endpoint for the food pantry profile page webpage
