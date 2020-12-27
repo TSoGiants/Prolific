29699e76d3b1d16ea2aab9ab6327ad788bdce5f0
onagbonoga <55672475+onagbonoga@users.noreply.github.com>
2020-12-26 19:44:42 -0600

    fixed libsass errors

--- prolificApp/__init__.py
+++ prolificApp/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from sassutils.wsgi import SassMiddleware
 app = Flask(__name__)
-app.wsgi_app = SassMiddleware(app.wsgi_app,{prolificApp:("static/styles", "static/styles", "/static/styles")})
+app.wsgi_app = SassMiddleware(app.wsgi_app,{'prolificApp':("static/styles", "static/styles", "/static/styles")})
 
 from prolificApp.user.views import user_app
 from prolificApp.foodPantry.views import foodPantry_app
