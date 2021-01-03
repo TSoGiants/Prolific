60838812f4ca74f212d50617027b0fc5425ff2ba
styx-sudo <62368485+styx-sudo@users.noreply.github.com>
2021-01-02 21:18:45 -0500

    updated

--- prolificApp/user/guRegistrationForm.py
+++ prolificApp/user/guRegistrationForm.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for, session, flash
 from flask_wtf import FlaskForm
-from wtforms import (StringField, IntegerField,SubmitField, validators)
+from wtforms import (StringField,IntegerField,SubmitField,validators)
 from wtforms.validators import DataRequired
 
 
