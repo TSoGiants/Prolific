59f3d81cf076b9b9c62fb6fff2be4d66874cd53f
styx-sudo <62368485+styx-sudo@users.noreply.github.com>
2021-01-16 20:22:39 -0500

    login code

--- prolificApp/user/fpRegistrationForm.py
+++ prolificApp/user/fpRegistrationForm.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, session, redirect, url_for, session, flash
 from flask_wtf import FlaskForm
-from wtforms import (StringField,
-                     SubmitField)
-from wtforms.validators import DataRequired
+from wtforms import (StringField,SubmitField,PasswordField,validators)
+from wtforms.validators import DataRequired,Email,EqualTo
 
 
 
 class AddFoodPantry(FlaskForm):
 
 #text in quotes is the label for the input box
-    fpname = StringField('Food Pantry Name')
-    fpemail = StringField("Food Pantry's Email")
-    fpaddress = StringField('Address')
-    fpphone = StringField("Food Pantry's Phone Number")
+    fpname = StringField('Food Pantry Name', validators = [DataRequired()])
+    fpemail = StringField("Food Pantry's Email", validators = [DataRequired(),Email()])
+    fpaddress = StringField('Address', validators = [DataRequired()])
+    fpphone = StringField("Food Pantry's Phone Number", validators = [DataRequired()])
     fpwebsite = StringField("Food Pantry's Website")
-    fppassword = StringField('Password')
-    fpconfirm = StringField('Confirm Password')
+    fppassword = PasswordField('Password', validators = [DataRequired(),EqualTo("fpconfirm",message='Passwords must match!')])
+    fpconfirm = PasswordField('Confirm Password', validators = [DataRequired()])
     submit = SubmitField('Sign Up')
+
+    def check_email(self, field):
+        # Check if not None for that user email!
+        if FoodPantries.query.filter_by(fpemail=field.data).first():
+            raise ValidationError('Your email has been registered already!')
+
+     
--- prolificApp/user/guLoginForm.py
+++ prolificApp/user/guLoginForm.py
@@ -2,3 +2,9 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from flask_login import LoginManager
 
+
+class guLoginForm(FlaskForm):
+    guemail = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
