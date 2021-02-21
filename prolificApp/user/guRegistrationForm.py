088a46ff8e04b820b6f02572565ced69b696836f
ayaanm0703 <ayaanm0703@gmail.com>
2021-02-20 20:16:39 -0600

    code cleaning/ back end fixes

--- prolificApp/user/guRegistrationForm.py
+++ prolificApp/user/guRegistrationForm.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, session, redirect, url_for, session, flash
 from flask_wtf import FlaskForm
-from wtforms import (StringField,IntegerField,SubmitField,PasswordField,validators)
+from wtforms import (StringField,IntegerField,SubmitField,PasswordField,validators,SelectField)
 from wtforms.validators import DataRequired,Email,EqualTo
 from wtforms import ValidationError
 import email_validator
@@ -11,7 +11,7 @@ class AddUser(FlaskForm):
 	gufirst = StringField('First Name', validators = [DataRequired()])
 	gulast = StringField("Last Name", validators = [DataRequired()])
 	guemail = StringField('Email', validators = [DataRequired(),Email()])
-	state = StringField("State", validators = [DataRequired()])
+	state = SelectField('State', validators = [DataRequired()], choices = ["Select State", "Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"])
 	zipcode = IntegerField("Zipcode", validators = [DataRequired()])
 	gupassword = PasswordField('Password', validators = [DataRequired(),EqualTo('guconfirm',message='Passwords must match!')])
 	guconfirm = PasswordField('Confirm Password', validators = [DataRequired()])
