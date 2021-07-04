f498770ddcb516500aa4efd5af9583f3e9ddd0bf
7styx-sudo <75050259+7styx-sudo@users.noreply.github.com>
2021-07-03 20:32:56 -0400

    Update editProfile.py

--- prolificApp/user/editProfile.py
+++ prolificApp/user/editProfile.py
@@ -6,7 +6,7 @@ import os
 from prolificApp.user.models import FoodPantries, Clients
 from prolificApp import db,app
 from flask_wtf import FlaskForm
-from wtforms import (StringField,SubmitField,PasswordField,validators)
+from wtforms import (StringField,SubmitField,PasswordField,validators, SelectField)
 from wtforms.validators import DataRequired,Email,EqualTo
 from prolificApp.user.models import FoodPantries, Clients
 from werkzeug.security import check_password_hash
@@ -17,7 +17,7 @@ class Editprofile(FlaskForm):
     fpname = StringField('Food Pantry Name')
     fpemail = StringField("Food Pantry's Email",  validators = [Email()])
     fpcity = StringField('City')
-    fpstate = SelectField({{ result.FPstate }}, choices = ["Select State", "Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"])
+    fpstate = SelectField("State", choices = ["Select State", "Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District ", "of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"])
     fpzipcode = StringField('Zipcode')
     fpstreet = StringField('Street Address')
     fpphone = StringField("Food Pantry's Phone Number")
